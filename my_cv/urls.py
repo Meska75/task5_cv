cd975ad8e7a99d87ba83c3a35b0f871406201d2e
Mohammad Eska <mohammad.eska34@gmail.com>
2025-05-28 12:39:34 +0330

    set cv and crate contact page

--- my_cv/urls.py
+++ my_cv/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from my_cv.views import index_view
-
+from my_cv.views import index_view , contact_view
 
 app_name = 'my_cv'
 
 urlpatterns = [
-    path('',index_view),
+    path('',index_view, name ='index'),
+    path('contact', contact_view , name = 'contact')
 ]
--- my_cv/views.py
+++ my_cv/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view (request):
-    return render(request , 'index.html')
+    return render(request , 'website/index.html')
+
+def contact_view (request):
+    contact_info = {'name': 'محمد اسکندرلو' , 'phone_number' : '+989337315709' , 'email': 'mohammad.eska34@gmail.com'}
+    return render(request , 'website/contact.html',contact_info)
